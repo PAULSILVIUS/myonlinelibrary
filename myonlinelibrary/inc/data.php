27da56f05ec7e296f76b2a6e91eea712d69d11bd
Paul Silvius <paulsilvius0@gmail.com>
2018-05-30 19:16:08 -0400

    Add files via upload

---
+++ myonlinelibrary/inc/data.php
@@ -0,0 +1,167 @@
+<?php
+$catalog = [];
+
+
+//Books
+
+$catalog[101] = [
+    "title" => "Breakfast of Champions",
+    "img" => "img/breakfast.jpg",
+   "genre" => "Scifi",
+   "format" => "Paperback",
+   "year" => 2000,
+   "category" => "Books",
+   "authors" => [
+       "Williamshakepears",
+       "14th Dalai Lama",
+       "Douglas Abrams"
+   ],
+   "publisher" => "Penguin Publishing Group",
+   "isbn" => '9780399185045'
+];
+
+$catalog[102] = [
+    "title" => "Fight Club",
+    "img" => "img/fightcb.jpg",
+   "genre" => "Drama",
+   "format" => "Paperback",
+   "year" => 2016,
+   "category" => "Books",
+   "authors" => [
+       "Desmond Tutu",
+       "14th Dalai Lama",
+       "Douglas Abrams"
+   ],
+   "publisher" => "Penguin Publishing Group",
+   "isbn" => '9780399185045'
+];
+
+$catalog[103] = [
+    "title" => "Post Office",
+    "img" => "img/post.jpg",
+   "genre" => "Fiction",
+   "format" => "Paperback",
+   "year" => 2016,
+   "category" => "Books",
+   "authors" => [
+       "Desmond Tutu",
+       "14th Dalai Lama",
+       "Douglas Abrams"
+   ],
+   "publisher" => "Penguin Publishing Group",
+   "isbn" => '9780399185045'
+];
+
+
+$catalog[104] = [
+    "title" => "Women",
+    "img" => "img/women.jpg",
+   "genre" => "Fiction",
+   "format" => "Paperback",
+   "year" => 2016,
+   "category" => "Books",
+   "authors" => [
+       "Desmond Tutu",
+       "14th Dalai Lama",
+       "Douglas Abrams"
+   ],
+   "publisher" => "Penguin Publishing Group",
+   "isbn" => '9780399185045'
+];
+
+//Movies
+
+$catalog[201] = [
+   "title" => "DeadPool",
+   "img" => "img/room.jpg",
+   "genre" => "Action,Adventure,Fantasy",
+   "format" => "DVD",
+   "year" => 2009,
+   "category" => "Movies",
+   "director" => "James Cameron",
+   "writers" => [
+       "James Cameron"
+
+   ],
+   "stars" => [
+       "Sam Worthington",
+       "Zoe Saldana",
+       "Sigourney Weaver"
+   ]
+];
+
+
+$catalog[202] = [
+   "title" => "Mr. Bean",
+   "img" => "img/3bill.jpg",
+   "genre" => "Comedy,Adventure",
+   "format" => "DVD",
+   "year" => 2009,
+   "category" => "Movies",
+   "director" => "James Cameron",
+   "writers" => [
+       "James Cameron"
+
+   ],
+   "stars" => [
+       "Sam Worthington",
+       "Zoe Saldana",
+       "Sigourney Weaver"
+   ]
+];
+
+
+$catalog[203] = [
+   "title" => "Transformers",
+   "img" => "img/her.jpg",
+   "genre" => "Action,Adventure,Fantasy",
+   "format" => "DVD",
+   "year" => 2009,
+   "category" => "Movies",
+   "director" => "James Cameron",
+   "writers" => [
+       "James Cameron"
+
+   ],
+   "stars" => [
+       "Sam Worthington",
+       "Zoe Saldana",
+       "Sigourney Weaver"
+   ]
+];
+
+
+
+//Music
+$catalog[301] = [
+   "title" => "Marcos Antonio Solis",
+   "img" => "img/damn.jpg",
+   "genre" => "Clasical, Romanti",
+   "format" => "CD",
+   "year" => 2012,
+   "category" => "Music",
+   "artist" => "Marcos Antonio Solis"
+];
+
+$catalog[302] = [
+   "title" => "Drake",
+   "img" => "img/fjm.jpg",
+   "genre" => "Hip-Hop",
+   "format" => "CD",
+   "year" => 2012,
+   "category" => "Music",
+   "artist" => "Drake"
+];
+
+
+$catalog[303] = [
+   "title" => "Complete Symphonies",
+   "img" => "img/pave.jpg",
+   "genre" => "Clasical",
+   "format" => "CD",
+   "year" => 2012,
+   "category" => "Music",
+   "artist" => "kennygmusic"
+];
+
+?>
